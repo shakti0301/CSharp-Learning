f8388bf7c8e17ba7a6b37365d3f9f6e21063296b
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-15 19:56:22 +0530

    Added EscapeSequences

---
+++ General/EscapeSequences/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EscapeSequences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    // New line example
+        Console.WriteLine("Hello\nWorld!");
+
+        // Tab example
+        Console.WriteLine("Hello\tWorld!");
+
+        // Double quotes inside string
+        Console.WriteLine("Hello \"C#\"!");
+
+        // Backslash example (file path)
+        Console.WriteLine("c:\\source\\repos");
+        }
+    }
+}
