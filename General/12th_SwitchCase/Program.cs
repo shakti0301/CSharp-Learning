88691ee91bd60eb032e60ddd337937a247821a66
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-21 17:25:23 +0530

    Added SwitchCaseDemo in General

---
+++ General/12th_SwitchCase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int marks = 85;
+        string grade;
+
+        switch (marks / 10)
+        {
+            case 10:
+            case 9:
+                grade = "A";
+                break;
+
+            case 8:
+                grade = "B";
+                break;
+
+            case 7:
+                grade = "C";
+                break;
+
+            case 6:
+                grade = "D";
+                break;
+
+            default:
+                grade = "F";
+                break;
+        }
+
+        Console.WriteLine($"Grade using switch: {grade}");
+    }
+}
