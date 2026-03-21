124cd7d8ed578eb3a0b16d3d54ac438ab6721e79
Shaktisinh Raolji <shaktiraolji@gmail.com>
2026-03-21 17:21:56 +0530

    Add ConditionDemo in General

--- General/11th_Condition/Program.cs
+++ General/11th_Condition/Program.cs
@@ -2,8 +2,32 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        
+        int marks = 85;
+        string grade;
+
+        if (marks >= 90)
+        {
+            grade = "A";
+        }
+        else if (marks >= 80)
+        {
+            grade = "B";
+        }
+        else if (marks >= 70)
+        {
+            grade = "C";
+        }
+        else if (marks >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Grade using if-else: {grade}");
     }
 }
